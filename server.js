739d23ff9b5afdd73749b1b72c1e07365380420d
svendoejaaren <svendoejaaren@hotmail.com>
2022-05-18 12:46:25 +0200

    Added routes and middleware

--- server.js
+++ server.js
@@ -4,13 +4,28 @@ const express = require('express')
 const app = express();
 const port = 1337;
 
+// Middleware
+app.use(express.static('public'))
+
 // routes
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+app.get('/register', (req, res) => {
+    res.send('Register here..... eventually')
+})
+
+app.get('/changepassword', (req, res) => {
+    res.send('Change your password here..... eventually')
+})
+
+// Ongeldige routes geven 404
+app.use( (req, res) => {
+    res.status(404).send("Error 404: page not found")
+})
 
 // server starten
 app.listen(port, () => {
-    console.log('Listening on port 1337');
+    console.log(`Listening on port ${port}`);
 })
